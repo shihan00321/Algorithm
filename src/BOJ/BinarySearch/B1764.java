4d1f1754eeabbec8cf20538de2a86dec16065126
shihan00321 <shihan001@naver.com>
2022-11-30 17:43:50 +0900

    <BOJ>1764 - 듣보잡

---
+++ src/BOJ/BinarySearch/B1764.java
@@ -0,0 +1,38 @@
+package BOJ.BinarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class B1764 {
+    static int N, M;
+    static ArrayList<String> arr = new ArrayList<>();
+    static HashSet<String> hashSet = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(arr.size());
+        Collections.sort(arr);
+        for (String arg : arr) {
+            System.out.println(arg);
+        }
+    }
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            hashSet.add(br.readLine());
+        }
+        for (int i = 0; i < M; i++) {
+            String s = br.readLine();
+            if(hashSet.contains(s)){
+                arr.add(s);
+            }
+        }
+    }
+}
