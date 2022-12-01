323dfefc94d7bb8e6cd3e994c9680df7fae52e5d
shihan00321 <shihan001@naver.com>
2022-12-01 19:10:14 +0900

    BaseCode create

---
+++ src/BaseCode.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaseCode {
+    public static void main(String[] args) throws IOException {
+        input();
+        algo();
+    }
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+    public static void algo(){
+
+    }
+}
