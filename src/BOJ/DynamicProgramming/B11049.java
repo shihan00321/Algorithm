c23238fae7a1b54cb33b3423204c5f0316db261a
shihan00321 <shihan001@naver.com>
2022-12-26 19:30:11 +0900

    <BOJ>1509 - 행렬 곱셈 순서 -> (실패) 다시 풀어보기

---
+++ src/BOJ/DynamicProgramming/B11049.java
@@ -0,0 +1,29 @@
+package BOJ.DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11049 {
+    static int N;
+    static int[] mul;
+    public static void main(String[] args) throws IOException {
+        input();
+        algo();
+    }
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        mul = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            mul[i] = x * y;
+        }
+    }
+    public static void algo(){
+
+    }
+}
