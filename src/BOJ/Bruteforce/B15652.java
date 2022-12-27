e6766ba09d33c5449bb578f75c5c0a1ca0535dad
shihan00321 <shihan001@naver.com>
2022-12-27 18:53:10 +0900

    <BOJ>15652 - N과 M (4)

---
+++ src/BOJ/Bruteforce/B15652.java
@@ -0,0 +1,40 @@
+package BOJ.Bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B15652 {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder stringBuilder = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        input();
+        algo(1);
+        System.out.println(stringBuilder);
+    }
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M + 1];
+    }
+    public static void algo(int depth){
+        if(depth == M + 1){
+            for (int i = 1; i <= M; i++) {
+                stringBuilder.append(arr[i]).append(' ');
+            }
+            stringBuilder.append('\n');
+        }
+        else {
+            for (int i = 1; i <= N; i++) {
+                if(depth != 1 && arr[depth - 1] > i) continue;
+                arr[depth] = i;
+                algo(depth + 1);
+                arr[depth] = 0;
+            }
+        }
+    }
+}
