e7b0222cb6edc7eba8ffc28093b03fe68d403032
shihan00321 <shihan001@naver.com>
2022-12-27 17:16:37 +0900

    BaseCode 코딩 테스트 유형

--- src/BaseCode.java
+++ src/BaseCode.java
@@ -4,6 +4,14 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BaseCode {
+    /**
+     * 코딩테스트 유형
+     * 완전 탐색 -> N개 중 중복을 허용o or x 해서 M개를 고르거나 나열하기
+     *
+     * 이분 탐색 -> 최대, 최소를 구하라
+     *
+     * 두 포인터 -> 연속 부분의 수열, 순서를 구하라, 곱 최소 ..
+     */
     public static void main(String[] args) throws IOException {
         input();
         algo();
