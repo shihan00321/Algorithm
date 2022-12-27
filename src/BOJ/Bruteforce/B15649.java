e89d87dca33e885b3608aceed9fe53cec7a7caf4
shihan00321 <shihan001@naver.com>
2022-12-27 18:12:47 +0900

    <BOJ>15649 - N과 M (1)

---
+++ src/BOJ/Bruteforce/B15649.java
@@ -0,0 +1,44 @@
+package BOJ.Bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B15649 {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visit;
+    static StringBuilder stringBuilder = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        input();
+        algo(1);
+        System.out.println(stringBuilder);
+    }
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M + 1];
+        visit = new boolean[N + 1];
+    }
+    public static void algo(int depth){
+        if(depth == M + 1){
+            for (int i = 1; i <= M; i++) {
+                stringBuilder.append(arr[i]).append(' ');
+            }
+            stringBuilder.append('\n');
+        }
+        else {
+            for (int i = 1; i <= N; i++) {
+                if(visit[i]) continue;
+                visit[i] = true;
+                arr[depth] = i;
+                algo(depth + 1);
+                visit[i] = false;
+                arr[depth] = 0;
+            }
+        }
+    }
+}
