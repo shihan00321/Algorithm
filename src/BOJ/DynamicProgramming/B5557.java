3dc3d5720e415778512f673d7f315385ac7e5374
shihan00321 <shihan001@naver.com>
2022-12-31 16:48:22 +0900

    <BOJ>5557 - 1학년

--- src/BOJ/DynamicProgramming/B5557.java
+++ src/BOJ/DynamicProgramming/B5557.java
@@ -7,30 +7,32 @@ import java.util.StringTokenizer;
 
 public class B5557 {
     static int N;
-    static int[] sum;
+    static int[] arr;
     static long[][] dy;
     public static void main(String[] args) throws IOException {
         input();
+        algo();
     }
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        sum = new int[N + 1];
-        dy = new long[N + 1][21];
+        arr = new int[N + 1];
+        dy = new long[N][21];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            sum[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        dy[1][sum[1]] = 1;
+    }
+    public static void algo(){
+        dy[1][arr[1]] = 1;
         for (int i = 2; i < N; i++) {
-            int target = sum[i];
             for (int j = 0; j < 21; j++) {
-                if(dy[i - 1][j] != 0) {
-                    if(j - target >= 0) dy[i][j - target] += dy[i - 1][j];
-                    if(j + target <= 20) dy[i][j + target] += dy[i - 1][j];
+                if(dy[i - 1][j] != 0){
+                    if(j - arr[i] >= 0) dy[i][j - arr[i]] += dy[i - 1][j];
+                    if(j + arr[i] <= 20) dy[i][j + arr[i]] += dy[i - 1][j];
                 }
             }
         }
-        System.out.println(dy[N - 1][sum[N]]);
+        System.out.println(dy[N - 1][arr[N]]);
     }
 }
