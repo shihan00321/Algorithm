f7d292d0e4b25db127c2cd431076711a8eb3629a
shihan00321 <shihan001@naver.com>
2022-12-31 16:23:21 +0900

    <BOJ>2156 - 포도주 시식

--- src/BOJ/DynamicProgramming/B2156.java
+++ src/BOJ/DynamicProgramming/B2156.java
@@ -16,26 +16,27 @@ public class B2156 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         wine = new int[n + 1];
+        //0 연속으로 마시는 경우, 1 직전의 잔을 마시지 않는 경우 or 2잔을 건너뛰고 마시는 경우 (o o x x o o)경우만 존재
         dy = new int[n + 1][2];
         for (int i = 1; i <= n; i++) {
             wine[i] = Integer.parseInt(br.readLine());
         }
     }
     public static void algo(){
-        //a,0 -> 직전 것을 마시는 경우,  a,1 -> i - 2 것을 마시는 경우 or i - 3을 마시는 경우
+        int result = Integer.MIN_VALUE;
         dy[1][0] = wine[1];
-        dy[1][1] = 0;
-        if(n >= 2){
-            dy[2][0] = wine[1] + wine[2];
+        if(n >= 2) {
+            dy[2][0] = dy[1][0] + wine[2];
             dy[2][1] = wine[2];
         }
         for (int i = 3; i <= n; i++) {
             dy[i][0] = dy[i - 1][1] + wine[i];
             dy[i][1] = Math.max(Math.max(dy[i - 2][0] + wine[i], dy[i - 2][1] + wine[i]), dy[i - 3][0] + wine[i]);
         }
-        int result = 0;
         for (int i = 1; i <= n; i++) {
-            result = Math.max(result, Math.max(dy[i][0], dy[i][1]));
+            for (int j = 0; j < 2; j++) {
+                result = Math.max(result, dy[i][j]);
+            }
         }
         System.out.println(result);
     }
