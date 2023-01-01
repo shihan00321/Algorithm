3f0b392a304f73dc3306f851c23f61bc578e63d4
shihan00321 <shihan001@naver.com>
2023-01-01 20:02:47 +0900

    <BOJ>2473 - 세 용액

--- src/BOJ/TwoPointers/B2473.java
+++ src/BOJ/TwoPointers/B2473.java
@@ -7,47 +7,41 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B2473 {
-    static int N;
-    static int[] sol;
-    static long result = Long.MAX_VALUE;
-    static int v1 = 0, v2 = 0, v3 = 0;
+    static int N, v1, v2, v3;
+    static int[] arr;
+    static StringBuilder stringBuilder = new StringBuilder();
     public static void main(String[] args) throws IOException {
         input();
         algo();
+        System.out.println(stringBuilder);
     }
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        sol = new int[N + 1];
+        arr = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            sol[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr, 1, N + 1);
     }
     public static void algo(){
-        Arrays.sort(sol, 1, N + 1);
+        long result = Long.MAX_VALUE;
         for (int i = 1; i <= N - 2; i++) {
-            minSolution(i);
+            int l = i + 1;
+            int r = N;
+            while (l < r){
+                //Math.abs(arr[i] + arr[l] + arr[r]) 값이 int 값을 벗어나므로 long 변환
+                if(result != Math.min(result, Math.abs(arr[l] + arr[r] + (long) arr[i]))){
+                    result = Math.min(result, Math.abs(arr[l] + arr[r] + (long) arr[i]));
+                    v1 = arr[i];
+                    v2 = arr[l];
+                    v3 = arr[r];
+                }
+                if(Math.abs(arr[i]) > Math.abs(arr[l] + arr[r])) l++;
+                else r--;
+            }
         }
-        StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(v1).append(' ').append(v2).append(' ').append(v3);
-        System.out.println(stringBuilder);
-    }
-
-    private static void minSolution(int index) {
-        int l = index + 1, r = N;
-        while (l < r) {
-            if(result != Math.min(result, Math.abs(sol[l] + sol[r] + (long) sol[index]))){
-                result = Math.min(result, Math.abs(sol[l] + sol[r] + (long) sol[index]));
-                v1 = sol[index];
-                v2 = sol[l];
-                v3 = sol[r];
-            }
-            if(sol[l] + sol[r] > Math.abs(sol[index])){
-                r--;
-            } else {
-                l++;
-            }
-        }
     }
 }
