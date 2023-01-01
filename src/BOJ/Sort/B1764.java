23ca114e5ecec4b9eb0e13dac51456400efbcff8
shihan00321 <shihan001@naver.com>
2023-01-01 17:21:30 +0900

    <BOJ>1764 - 듣보잡

--- src/BOJ/Sort/B1764.java
+++ src/BOJ/Sort/B1764.java
@@ -5,20 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class B1764 {
     static int N, M;
-    static ArrayList<String> arr = new ArrayList<>();
-    static HashSet<String> hashSet = new HashSet<>();
+    static HashMap<String, Integer> hashMap = new HashMap<>();
+    static StringBuilder stringBuilder = new StringBuilder();
     public static void main(String[] args) throws IOException {
         input();
-        System.out.println(arr.size());
-        Collections.sort(arr);
-        for (String arg : arr) {
-            System.out.println(arg);
-        }
     }
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,13 +21,18 @@ public class B1764 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         for (int i = 0; i < N; i++) {
-            hashSet.add(br.readLine());
+            hashMap.put(br.readLine(), 1);
         }
+        ArrayList<String> arr = new ArrayList<>();
         for (int i = 0; i < M; i++) {
             String s = br.readLine();
-            if(hashSet.contains(s)){
-                arr.add(s);
-            }
+            if(hashMap.containsKey(s)) arr.add(s);
         }
+        Collections.sort(arr);
+        stringBuilder.append(arr.size()).append('\n');
+        for (String s : arr) {
+            stringBuilder.append(s).append('\n');
+        }
+        System.out.println(stringBuilder);
     }
 }
