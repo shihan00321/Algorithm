139456f535f0376b58d1fce7f3ea4271d718a53e
shihan00321 <shihan001@naver.com>
2023-01-02 19:15:03 +0900

    <BOJ>1182 - 부분수열의 합

--- src/BOJ/Bruteforce/B1182.java
+++ src/BOJ/Bruteforce/B1182.java
@@ -10,8 +10,8 @@ public class B1182 {
     static int[] arr;
     public static void main(String[] args) throws IOException {
         input();
-        algo(1, 0);
-        if(S == 0) System.out.println(--result);
+        algo(0, 0);
+        if(S == 0) System.out.println(result - 1);
         else System.out.println(result);
     }
     public static void input() throws IOException {
@@ -19,19 +19,19 @@ public class B1182 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
-        arr = new int[N + 1];
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
     }
-    public static void algo(int depth, int cal){
-        if(depth == N + 1) {
-            if(cal == S) result++;
+    public static void algo(int depth, int value){
+        if(depth == N) {
+            if (value == S) result++;
         }
         else {
-            algo(depth + 1, cal + arr[depth]);
-            algo(depth + 1, cal);
+            algo(depth + 1, value + arr[depth]);
+            algo(depth + 1, value);
         }
     }
 }
