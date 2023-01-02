d9dd37de992978dc405cedcc8cd0b10914fc5958
shihan00321 <shihan001@naver.com>
2023-01-02 18:53:01 +0900

    <BOJ>1509 - 팰린드롬 분할

--- src/BOJ/DynamicProgramming/B1509.java
+++ src/BOJ/DynamicProgramming/B1509.java
@@ -5,34 +5,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class B1509 {
-    static String s;
     static int N;
+    static String s;
     static int[][] dy;
     static int[] palindrome;
     public static void main(String[] args) throws IOException {
         input();
+        algo();
     }
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         s = br.readLine();
         N = s.length();
-        algo();
-    }
-    public static void algo(){
         dy = new int[N][N];
         palindrome = new int[N];
         for (int i = 0; i < N; i++) {
             dy[i][i] = 1;
             palindrome[i] = Integer.MAX_VALUE;
         }
-        for (int i = 1; i < N; i++) {
-            if(s.charAt(i - 1) == s.charAt(i)) dy[i - 1][i] = 1;
-            else dy[i - 1][i] = 0;
-        }
-        for (int len = 3; len <= N; len++) {
+    }
+    public static void algo(){
+        for (int len = 2; len <= N; len++) {
             for (int i = 0; i <= N - len; i++) {
                 int j = len + i - 1;
-                if(s.charAt(i) == s.charAt(j) && dy[i + 1][j - 1] == 1) dy[i][j] = 1;
+                if(s.charAt(i) == s.charAt(j)) {
+                    if(len == 2) dy[i][j] = 1;
+                    else { if(dy[i + 1][j - 1] == 1) dy[i][j] = 1; }
+                }
             }
         }
         for (int i = 0; i < N; i++) {
