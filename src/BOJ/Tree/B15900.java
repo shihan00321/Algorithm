b6a8f86b44c6237a522e1e3648d3a2a0d6d621a2
shihan00321 <shihan001@naver.com>
2023-01-02 19:54:58 +0900

    <BOJ>15900 - 나무 탈출

--- src/BOJ/Tree/B15900.java
+++ src/BOJ/Tree/B15900.java
@@ -7,36 +7,37 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class B15900 {
-    static int N;
-    static int count = 0;
+    static int N, sumDepths;
     static ArrayList<Integer>[] arr;
     public static void main(String[] args) throws IOException {
         input();
         dfs(1, -1, 0);
-        if(count % 2 == 0) System.out.println("No");
+        if(sumDepths % 2 == 0) System.out.println("No");
         else System.out.println("Yes");
-    }
-    private static void dfs(int target, int parent, int cnt) {
-        //leafNode -> 간선이 1개.
-        if(arr[target].size() == 1 && target != 1) {
-            count += cnt;
-            return;
-        }
-        for(int x : arr[target])
-            if(x != parent)
-                dfs(x, target, cnt + 1);
     }
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) arr[i] = new ArrayList<>();
-        for (int i = 1; i <= N - 1; i++) {
+        for (int i = 1; i <= N; i++) {
+            arr[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             arr[x].add(y);
             arr[y].add(x);
+        }
+    }
+    public static void dfs(int target, int par, int depth){
+        if(arr[target].size() == 1 && target != 1) {
+            sumDepths += depth;
+            return;
+        }
+        for (int y : arr[target]) {
+            if(y == par) continue;
+            dfs(y, target, depth + 1);
         }
     }
 }
