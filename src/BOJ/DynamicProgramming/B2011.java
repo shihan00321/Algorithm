8c164c84a43bd922176ea1171651c89230ff5fe1
shihan00321 <shihan001@naver.com>
2023-01-02 17:26:16 +0900

    <BOJ>2011 - 암호코드

--- src/BOJ/DynamicProgramming/B2011.java
+++ src/BOJ/DynamicProgramming/B2011.java
@@ -11,6 +11,9 @@ public class B2011 {
     public static void main(String[] args) throws IOException {
         input();
         System.out.println(dy[size - 1]);
+        for (int i = 0; i < size; i++) {
+            System.out.println(dy[i]);
+        }
     }
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,11 +23,11 @@ public class B2011 {
         for (int i = 1; i < size; i++) {
             // 현재 숫자를 단독으로 해석 가능하다면 전의 경우의 수와 같음
             if (code.charAt(i) != '0') dy[i] = dy[i - 1];
-            // 앞 숫자와 하나의 문자로 해석이 가능하다면? -> 1가지 경우가 더 추가
+            // 앞 숫자와 하나의 문자로 해석이 가능하다면? -> i - 2번의 경우에서 문자를 해석하거나 i - 1번의 경우에서 문자를 해석 할 수 있음.
             if (checkCode(code.charAt(i - 1), code.charAt(i))) {
                 if (i >= 2) dy[i] += dy[i - 2];
                 else dy[i] += 1;
-                dy[i] %= 1000000; // 위에서 연산할 때는 안되는 이유?
+                dy[i] %= 1000000;
             }
         }
     }
