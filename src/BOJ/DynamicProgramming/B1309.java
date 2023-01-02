9eaef6c2ad3b1f37a417f0db3c036fdfcff7cbb3
shihan00321 <shihan001@naver.com>
2023-01-02 19:03:42 +0900

    <BOJ>1309 - 동물원

--- src/BOJ/DynamicProgramming/B1309.java
+++ src/BOJ/DynamicProgramming/B1309.java
@@ -10,22 +10,22 @@ public class B1309 {
     public static void main(String[] args) throws IOException {
         input();
         algo();
+        System.out.println((zoo[N][0] + zoo[N][1] + zoo[N][2]) % 9901);
     }
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        //a,0 아무 것도 없는 경우, a,1 왼쪽에 사자가 있는 경우, a,2 오른쪽에 사자가 있는 경우
+        //0 사자가 없는 경우, 1 사자가 왼쪽에 있는 경우, 2 사자가 오른쪽에 있는 경우
         zoo = new int[N + 1][3];
-    }
-    public static void algo(){
         zoo[1][0] = 1;
         zoo[1][1] = 1;
         zoo[1][2] = 1;
+    }
+    public static void algo(){
         for (int i = 2; i <= N; i++) {
             zoo[i][0] = (zoo[i - 1][0] + zoo[i - 1][1] + zoo[i - 1][2]) % 9901;
             zoo[i][1] = (zoo[i - 1][0] + zoo[i - 1][2]) % 9901;
             zoo[i][2] = (zoo[i - 1][0] + zoo[i - 1][1]) % 9901;
         }
-        System.out.println((zoo[N][0] + zoo[N][1] + zoo[N][2]) % 9901);
     }
 }
