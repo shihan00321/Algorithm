1cbc8f3a9f44a370bf4826b91c52c0f62a396fc2
shihan00321 <shihan001@naver.com>
2023-01-03 18:03:50 +0900

    <BOJ>7795 - 먹을 것인가 먹힐 것인가

--- src/BOJ/BinarySearch/B7795.java
+++ src/BOJ/BinarySearch/B7795.java
@@ -6,18 +6,24 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class B7795 {
-    public static int N, M;
-    public static int[] A, B;
+    static int T, N, M;
+    static int[] A, B;
+    static StringBuilder stringBuilder = new StringBuilder();
     public static void main(String[] args) throws IOException {
         input();
+        System.out.println(stringBuilder);
     }
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //테스트 케이스
-        int T;
         T = Integer.parseInt(br.readLine());
-        while (T-- > 0){
+        while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
@@ -31,30 +37,22 @@ public class B7795 {
             for (int i = 0; i < M; i++) {
                 B[i] = Integer.parseInt(st.nextToken());
             }
-            algo();
-        }
-    }
-    public static int lower_bound(int[] B, int l, int r, int x){
-        int result = 0;
-        while (l <= r){
-            int mid = (l + r) / 2;
-            if(B[mid] < x) {
-                //index = 0부터 시작이므로 개수는 + 1을 해주어야 함
-                result = mid + 1;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+            Arrays.sort(B);
+            int result = 0;
+            for (int i = 0; i < N; i++) {
+                result += lower_bound(A[i]);
             }
+            stringBuilder.append(result).append('\n');
         }
-        return result;
     }
-    public static void algo(){
-        Arrays.sort(B);
-        int result = 0;
-        for (int i = 0; i < N; i++) {
-            //배열 B에서 A[i]보다 작은 것이 몇 개 있는지?
-            result += lower_bound(B, 0, M-1, A[i]);
+    private static int lower_bound(int target) {
+        int l = 0, r = M - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if(target <= B[mid]) r = mid - 1;
+            else l = mid + 1;
         }
-        System.out.println(result);
+        //l이 최종적으로 움직인 좌표까지가 먹을 수 있음 -> 실질적으로 l - 1까지 먹을 수 있지만 index 시작이 0 이므로 l + 1과 같음
+        return l;
     }
 }
