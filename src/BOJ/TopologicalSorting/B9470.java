5b565a6abd99ace31fe446f45ba2d2df12b66e20
shihan00321 <shihan001@naver.com>
2023-01-03 20:06:16 +0900

    <BOJ>9470 - Strahler 순서

--- src/BOJ/TopologicalSorting/B9470.java
+++ src/BOJ/TopologicalSorting/B9470.java
@@ -10,9 +10,9 @@ import java.util.StringTokenizer;
 
 public class B9470 {
     static int T, K, M, P;
-    static StringBuilder stringBuilder = new StringBuilder();
     static ArrayList<Integer>[] arr;
     static int[] inDegree, strahler, count;
+    static StringBuilder stringBuilder = new StringBuilder();
     public static void main(String[] args) throws IOException {
         input();
         System.out.println(stringBuilder);
@@ -20,15 +20,15 @@ public class B9470 {
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
-        while (T-- > 0){
+        while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             K = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             P = Integer.parseInt(st.nextToken());
             arr = new ArrayList[M + 1];
             inDegree = new int[M + 1];
-            count = new int[M + 1];
             strahler = new int[M + 1];
+            count = new int[M + 1];
             for (int i = 1; i <= M; i++) {
                 arr[i] = new ArrayList<>();
             }
@@ -45,15 +45,15 @@ public class B9470 {
     public static void algo(){
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 1; i <= M; i++) {
-            if (inDegree[i] == 0) {
-                queue.add(i);
+            if(inDegree[i] == 0) {
                 strahler[i] = 1;
                 count[i] = 1;
+                queue.add(i);
             }
         }
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int x = queue.poll();
-            for(int y : arr[x]){
+            for(int y : arr[x]) {
                 inDegree[y]--;
                 //이미 strahler 순서의 강물이 한번도 들어오지 않은 경우 or 두 가지 이상의 strahler 강물이 들어와 +1이 된 경우
                 //순서가 더 큰 강물만 확인하면 된다.
@@ -62,25 +62,19 @@ public class B9470 {
                     strahler[y] = Math.max(strahler[y], strahler[x]);
                 }
                 else {
-                    //이미 들어온 강물이 한번 더 들어왔을 때
                     if(strahler[y] == strahler[x]) {
                         strahler[y]++;
                         count[y] = 0;
                     }
-                    //다른 강물이 들어왔을 때
-                    else {
-                        strahler[y] = Math.max(strahler[y], strahler[x]);
-                    }
+                    else strahler[y] = Math.max(strahler[y], strahler[x]);
                 }
-                if(inDegree[y] == 0) {
-                    queue.add(y);
-                }
+                if(inDegree[y] == 0) queue.add(y);
             }
         }
-        int result = 0;
+        int max = 0;
         for (int i = 1; i <= M; i++) {
-            result = Math.max(result, strahler[i]);
+            max = Math.max(max, strahler[i]);
         }
-        stringBuilder.append(K).append(' ').append(result).append('\n');
+        stringBuilder.append(K).append(' ').append(max).append('\n');
     }
 }
