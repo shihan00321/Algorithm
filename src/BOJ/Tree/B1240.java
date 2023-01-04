38de5e7eb711a5ad5c678ed58cf15f40ddae1eba
shihan00321 <shihan001@naver.com>
2023-01-04 17:07:32 +0900

    <BOJ>1240 - 노드사이의 거리

--- src/BOJ/Tree/B1240.java
+++ src/BOJ/Tree/B1240.java
@@ -10,11 +10,11 @@ public class B1240 {
     static int N, M;
     static ArrayList<Node>[] arr;
     static StringBuilder stringBuilder = new StringBuilder();
-    static class Node{
-        int edge;
+    static class Node {
+        int dest;
         int weight;
-        public Node(int edge, int weight) {
-            this.edge = edge;
+        public Node(int dest, int weight) {
+            this.dest = dest;
             this.weight = weight;
         }
     }
@@ -35,25 +35,25 @@ public class B1240 {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            arr[x].add(new Node(y, weight));
-            arr[y].add(new Node(x, weight));
+            int w = Integer.parseInt(st.nextToken());
+            arr[x].add(new Node(y, w));
+            arr[y].add(new Node(x, w));
         }
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int target1 = Integer.parseInt(st.nextToken());
-            int target2 = Integer.parseInt(st.nextToken());
-            dfs(target1, 0, target2, 0);
+            int targetA = Integer.parseInt(st.nextToken());
+            int targetB = Integer.parseInt(st.nextToken());
+            dfs(targetA, targetB, 0, -1);
         }
     }
-    private static void dfs(int target1, int parents, int target2, int depth) {
-        if(target1 == target2) {
-            stringBuilder.append(depth).append('\n');
+    public static void dfs(int A, int B , int weight, int parent){
+        if(A == B) {
+            stringBuilder.append(weight).append('\n');
             return;
         }
-        for(Node node : arr[target1]) {
-            if(node.edge == parents) continue;
-            dfs(node.edge, target1, target2, depth + node.weight);
+        for(Node node : arr[A]) {
+            if(node.dest == parent) continue;
+            dfs(node.dest, B, weight + node.weight, A);
         }
     }
 }
