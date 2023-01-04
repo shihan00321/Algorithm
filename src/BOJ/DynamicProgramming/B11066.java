4110fcc3f676670b78a0b2d29b22c41d7ff39e10
shihan00321 <shihan001@naver.com>
2023-01-04 19:04:22 +0900

    <BOJ>11066 - 파일 합치기

--- src/BOJ/DynamicProgramming/B11066.java
+++ src/BOJ/DynamicProgramming/B11066.java
@@ -7,46 +7,45 @@ import java.util.StringTokenizer;
 
 public class B11066 {
     static int T, K;
-    static int[] arr;
+    static int[] file;
     static int[][] dy, sum;
     static StringBuilder stringBuilder = new StringBuilder();
     public static void main(String[] args) throws IOException {
         input();
+        System.out.println(stringBuilder);
     }
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
-        while (T-- > 0){
+        while (T-- > 0 ){
             K = Integer.parseInt(br.readLine());
-            arr = new int[K + 1];
-            //sum[i, j] -> i에서 j까지 합
-            sum = new int[K + 1][K + 1];
-            dy = new int[K + 1][K + 1];
+            dy = new int[K][K];
+            sum = new int[K][K];
+            file = new int[K];
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= K; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
-                sum[i][i] = arr[i];
+            for (int i = 0; i < K; i++) {
+                file[i] = Integer.parseInt(st.nextToken());
+                sum[i][i] = file[i];
             }
-            for (int i = 1; i <= K; i++) {
-                for (int j = 1; j <= K; j++) {
-                    if(i == j || i > j) continue;
-                    sum[i][j] = sum[i][j - 1] + arr[j];
+            for (int i = 0; i < K; i++) {
+                for (int j = i; j < K; j++) {
+                    if(j == 0) continue;
+                    sum[i][j] = sum[i][j - 1] + file[j];
                 }
             }
-            //구할 길이 -> len = 2라면 (1, 2), (2, 3), (3, 4)을 나타냄
-            for (int len = 2; len <= K; len++) {
-                //행
-                for (int i = 1; i <= K - len + 1; i++) {
-                    //열
-                    int j = i + len - 1;
-                    dy[i][j] = Integer.MAX_VALUE;
-                    for (int k = i; k < j; k++) {
-                        dy[i][j] = Math.min(dy[i][j], dy[i][k] + dy[k + 1][j] + sum[i][j]);
-                    }
-                }
-            }
-            stringBuilder.append(dy[1][K]).append('\n');
+            algo();
         }
-        System.out.println(stringBuilder);
+    }
+    public static void algo(){
+        for (int len = 2; len <= K; len++) {
+            for (int i = 0; i <= K - len; i++) {
+                int j = i + len - 1;
+                dy[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    dy[i][j] = Math.min(dy[i][j], dy[i][k] + dy[k + 1][j] + sum[i][j]);
+                }
+            }
+        }
+        stringBuilder.append(dy[0][K - 1]).append('\n');
     }
 }
