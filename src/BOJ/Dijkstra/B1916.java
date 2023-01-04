40bd28970208610375fe97f1a3c9ee72064ab510
shihan00321 <shihan001@naver.com>
2023-01-04 20:00:05 +0900

    <BOJ>1916 - 최소비용 구하기

--- src/BOJ/Dijkstra/B1916.java
+++ src/BOJ/Dijkstra/B1916.java
@@ -8,61 +8,58 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class B1916 {
-    static int N, M, resultS, resultD;
-    static int[] dist; //목적지까지의 거리
-    static ArrayList<BusInfo>[] bus;
-    static class BusInfo{
-        int destination, weight;
-        public BusInfo(int destination, int weight) {
-            this.destination = destination;
-            this.weight = weight;
-        }
-    }
-    static class ShortestPath {
-        int destination, distance;
-        public ShortestPath(int destination, int distance) {
-            this.destination = destination;
-            this.distance = distance;
-        }
-    }
+    static int N, M;
+    static int[] dist;
+    static ArrayList<Bus>[] arr;
     public static void main(String[] args) throws IOException {
         input();
-        algo();
-        System.out.println(dist[resultD]);
     }
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
+        arr = new ArrayList[N + 1];
         dist = new int[N + 1];
-        bus = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
-            bus[i] = new ArrayList<>();
+            arr[i] = new ArrayList<>();
             dist[i] = Integer.MAX_VALUE;
         }
         for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int destination = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            bus[start].add(new BusInfo(destination, weight));
+            arr[Integer.parseInt(st.nextToken())].add(new Bus(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
         StringTokenizer st = new StringTokenizer(br.readLine());
-        resultS = Integer.parseInt(st.nextToken());
-        resultD = Integer.parseInt(st.nextToken());
+        algo(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
     }
-    public static void algo(){
-        PriorityQueue<ShortestPath> queue = new PriorityQueue<>((o1, o2) -> o1.distance - o2.distance);
-        dist[resultS] = 0;
-        queue.add(new ShortestPath(resultS, 0));
-        while (!queue.isEmpty()){
-            ShortestPath path = queue.poll();
-            if(dist[path.destination] < path.distance) continue;
-            for(BusInfo busInfo : bus[path.destination]){
-                if(dist[busInfo.destination] <= dist[path.destination] + busInfo.weight) continue;
-                dist[busInfo.destination] = dist[path.destination] + busInfo.weight;
-                queue.add(new ShortestPath(busInfo.destination, dist[busInfo.destination]));
+    public static void algo(int x, int y){
+        PriorityQueue<Path> pq = new PriorityQueue<>((o1, o2) -> o1.distance - o2.distance);
+        dist[x] = 0;
+        pq.add(new Path(x, 0));
+        while (!pq.isEmpty()){
+            Path path = pq.poll();
+            if(dist[path.busNum] < path.distance) continue;
+            for(Bus nextBus : arr[path.busNum]){
+                if(dist[nextBus.dest] <= dist[path.busNum] + nextBus.cost) continue;
+                dist[nextBus.dest] = dist[path.busNum] + nextBus.cost;
+                pq.add(new Path(nextBus.dest, dist[nextBus.dest]));
             }
+        }
+        System.out.println(dist[y]);
+    }
+    static class Bus {
+        int dest;
+        int cost;
+        public Bus(int dest, int cost) {
+            this.dest = dest;
+            this.cost = cost;
+        }
+    }
+    static class Path {
+        int busNum;
+        int distance;
+        public Path(int busNum, int distance) {
+            this.busNum = busNum;
+            this.distance = distance;
         }
     }
 }
