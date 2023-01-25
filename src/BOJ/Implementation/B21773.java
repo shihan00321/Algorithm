81244bc704d0edc485926ae0730dee79b45b98a3
shihan00321 <shihan001@naver.com>
2023-01-25 16:46:42 +0900

    <BOJ>21773 - 가희와 프로세스 1

--- src/BOJ/Implementation/B21773.java
+++ src/BOJ/Implementation/B21773.java
@@ -41,6 +41,7 @@ public class B21773 {
         }
         for (int i = 0; i < T; i++) {
             Process process = queue.poll();
+            assert process != null;
             stringBuilder.append(process.id).append('\n');
             process.time--;
             process.priority--;
